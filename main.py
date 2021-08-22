507353582c42e564251ebc4c25679289bc5ec7a9
g-battaglia <battaglia.giacomo@icloud.com>
2021-08-22 19:56:55 +0200

    Bugfix.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from typing import Optional
 from fastapi import FastAPI
 from functions import get_tabs
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
